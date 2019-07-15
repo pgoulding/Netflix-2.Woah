1710c1069a3be39ec06c8463c43fea85ede97f65
Katherine Lewis <kale8432@colorado.edu>
2019-07-15 17:31:51 -0600

    setting up movie reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { combineReducers } from 'redux';
+import {
+	combineReducers
+} from 'redux';
 import MoviesReducer from './MoviesReducer';
 import FavoritesReducer from './FavoritesReducer';
 import UserSignInReducer from './UserSignInReducer';
@@ -7,5 +9,5 @@ import UserSignInReducer from './UserSignInReducer';
 export const rootReducer = combineReducers({
 	movies: MoviesReducer,
 	favorites: FavoritesReducer,
-	signin: UserSignInReducer
+	signin: UserReducer
 });
