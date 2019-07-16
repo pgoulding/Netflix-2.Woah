8166aebab6c2e48e13dcb2627f1df84f8d5b0611
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-16 11:16:29 -0600

    add cleaner function to default movies
    Co-authored-by: Katherine Lewis <Kale8432@colorado.edu>
    Co-authored-by: Patrick Goulding <patrickgoulding@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/ApiFetch.js
+++ src/ApiFetch.js
@@ -1,15 +1,21 @@
 // import { getAllMovies } from './actions';
 import apiKey from './apikey'
+import {cleanDefaultCategories} from './utils/cleanerFunction'
 
-const getData = async () => {
-  const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
-  try {
-    const response = await fetch(nowPlaying);
-    const parsed = await response.json()
-    return parsed.results
-  } catch (error) {
-    throw Error(error.message);
-  }
+const getDefaultData = async () => {
+  const movieList = ['now_playing', 'popular', 'top_rated']
+  return Promise.all(movieList.map(async movie => {
+    try {
+      const nowPlaying = `https://api.themoviedb.org/3/movie/${movie}?api_key=${apiKey}`;
+      const response = await fetch(nowPlaying);
+      const parsed = await response.json()
+      const cleaned = await cleanDefaultCategories(movie, parsed.results)
+      return cleaned
+    } catch (error) {
+      throw Error(error.message);
+    }
+  }))
 };
 
-export default getData;
+
+export {getDefaultData};
