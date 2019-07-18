33e745bd98a153e1ea75a6f77bb00132ac304113
Katherine Lewis <kale8432@colorado.edu>
2019-07-18 11:14:56 -0600

    add reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,25 @@
 // import React from 'react';
-import { combineReducers } from 'redux';
-import { MoviesReducer }from './MoviesReducer';
+import {
+	combineReducers
+} from 'redux';
+import {
+	getMoviesReducer
+} from './getMoviesReducer';
 // import FavoritesReducer from './FavoritesReducer';
 import UserReducer from './UserReducer';
+import {
+	throwErrorReducer
+} from './throwErrorReducer';
+import {
+	isLoadingReducer
+} from './isLoadingReducer';
 
 const rootReducer = combineReducers({
-	movies: MoviesReducer,
+	movies: getMoviesReducer,
 	// favorites: FavoritesReducer,
-	user: UserReducer
+	user: UserReducer,
+	isLoading: isLoadingReducer,
+	throwError: throwErrorReducer
 });
 
 export default rootReducer
