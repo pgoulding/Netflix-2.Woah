3c4f9768695fbcf794926149a2ab59a860c328a0
Katherine Lewis <kale8432@colorado.edu>
2019-07-18 16:10:03 -0600

    removed genre from cleaner

--- src/utils/cleanerFunction.js
+++ src/utils/cleanerFunction.js
@@ -1,22 +1,20 @@
-export const cleanDefaultMovies = (genre, results) => {
+export const cleanDefaultMovies = (results) => {
   const newResults = results.map(movieData => {
     return {
       ...movieData,
       movie_id: movieData.id,
       isFavorited: false,
-      genre
     }
   })
   return [...newResults]
 };
 
-export const cleanCategoryMovies = (genre, results) => {
+export const cleanCategoryMovies = (results) => {
   const newResults = results.map(movieData => {
     return {
       ...movieData,
       movie_id: movieData.id,
       isFavorited: false,
-      genre
     }
   })
   return [...newResults]
