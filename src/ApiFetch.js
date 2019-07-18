40c80a32f1948df360475e0304a39be727d8526a
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-17 18:04:24 -0600

    add create user and signin user fetch's
    Co-authored-by: Katherine Lewis <Kale8432@colorado.edu>
    Co-authored-by: Patrick Goulding <patrickgoulding@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/ApiFetch.js
+++ src/ApiFetch.js
@@ -1,15 +1,17 @@
 // import { getAllMovies } from './actions';
 import apiKey from './apikey'
-import {cleanDefaultCategories} from './utils/cleanerFunction'
+import { cleanDefaultCategories } from './utils/cleanerFunction'
+
+// const localUser = 'http://localhost:3001/api/users/'
 
 const getDefaultData = async () => {
   const movieList = ['now_playing', 'popular', 'top_rated']
-  return Promise.all(movieList.map(async movie => {
+  return Promise.all(movieList.map(async genre => {
     try {
-      const nowPlaying = `https://api.themoviedb.org/3/movie/${movie}?api_key=${apiKey}`;
+      const nowPlaying = `https://api.themoviedb.org/3/movie/${genre}?api_key=${apiKey}`;
       const response = await fetch(nowPlaying);
       const parsed = await response.json()
-      const cleaned = await cleanDefaultCategories(movie, parsed.results)
+      const cleaned = await cleanDefaultCategories(genre, parsed.results)
       return cleaned
     } catch (error) {
       throw Error(error.message);
@@ -17,5 +19,44 @@ const getDefaultData = async () => {
   }))
 };
 
+const sendUserLogin = async (email, password) => {
+  const user = {
+    email,
+    password
+  }
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    }
+    const userSignInURL = 'http://localhost:3001/api/users/'
+    const response = await fetch(userSignInURL, options)
+    const parsed = await response.json()
+    return parsed
+  } catch (error) {
+    throw Error('we have encoutnered an error: ', error)
+  }
+} 
 
-export {getDefaultData};
+const sendNewAccount = async (newAccount) => {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(newAccount)
+    }
+    const newUserUrl = 'http://localhost:3001/api/users/new'
+    const response = await fetch(newUserUrl, options)
+    return response
+  } catch(error) {
+    throw Error('we have encoutnered an error: ', error)
+  }
+}
+
+
+export {getDefaultData, sendNewAccount, sendUserLogin};
