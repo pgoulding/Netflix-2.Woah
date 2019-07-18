ad689fbb7d3ae8e6f11e4c07220f3c701aa901ed
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-17 22:24:14 -0600

    add conditional rendering and onClick method to send data to api call

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './Card.css'
-const Card = ({ movieInfo }) => {
-  const { title, poster_path } = movieInfo
+import { sendFavorite } from '../../ApiFetch'
 
+const Card = ({ movieInfo, user }) => {
+  const { title, poster_path, movie_id, release_date, vote_average, overview } = movieInfo
+  const { user_id } = user
   const imageUrl = `http://image.tmdb.org/t/p/w300/${poster_path}`
   return (
     <article className="card">
       <h3>{ title }</h3>
       <img alt={ title && ' movie poster' } src={ imageUrl }/>
+      {user.id && <button onClick={() => sendFavorite({ movie_id, user_id, title, poster_path, release_date, vote_average, overview})}>Favorite Movie</button>}
     </article>
   )
 }
 
-export default Card
+const mapStateToProps = ({ user }) => ({
+  user
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  // signIn: (userInfo) => dispatch(signIn(userInfo))
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card)
