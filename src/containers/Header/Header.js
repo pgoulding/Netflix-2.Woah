886a342a672bbc1ea6613c621b228bfb7a8f5432
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-19 18:01:26 -0600

    add user components to display in header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Header.css'
+import UserMenu from '../UserMenu/UserMenu'
+
 const Header = () => {
   return (
     <header>
       <h1>BetterFlix</h1>
+      <UserMenu />
     </header>
   )
 }
