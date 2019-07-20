b7ac610f11edd416e00d5e398be963740dba555e
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-19 20:30:34 -0600

    add Detailed movie component for on click

---
+++ src/containers/DetailedMovieCard/DetailedMovieCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DetailedMovieCard = (movie) => {
+  const { title, imageUrl, overview, vote_count, vote_average, genre_ids} = movie
+  const 
+  return (
+    <div>
+      <h3>{title}</h3>
+      <img alt={overview} src={imageUrl}/>
+      <p>{overview}</p>
+      <p>Rating: {vote_average} /10</p>
+      <p>Times Reviewed: {vote_count}</p>
+    </div>
+  )
+}
+
+export default DetailedMovieCard
