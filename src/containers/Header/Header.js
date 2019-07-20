16d77c6c572a66e7b27c3175e9031780282d08c3
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-19 20:36:47 -0600

    add search component to header
    Co-authored-by: Katherine Lewis <Kale8432@colorado.edu>
    Co-authored-by: Patrick Goulding <patrickgoulding@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css'
 import UserMenu from '../UserMenu/UserMenu'
-
+import Search from '../Search/Search'
 const Header = () => {
   return (
     <header>
       <h1>BetterFlix</h1>
       <UserMenu />
+      <Search/>
     </header>
   )
 }
