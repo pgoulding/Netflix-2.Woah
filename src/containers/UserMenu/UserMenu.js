b5ece004a72927f6396f9b9037044456d74b9507
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-20 13:09:26 -0600

    fix dispatch to props

--- src/containers/UserMenu/UserMenu.js
+++ src/containers/UserMenu/UserMenu.js
@@ -7,14 +7,14 @@ import { signOut } from '../../actions';
 export class UserMenu extends Component {
   signOutUser = async e => {
     e.preventDefault();
-    await this.setState({
-      name: '',
-      password: '',
-      email: ''
-    });
+    // await this.setState({
+    //   name: '',
+    //   password: '',
+    //   email: ''
+    // });
     await this.props.signOut();
-    console.log(this.props.signOut({ ...this.props.user }));
-    console.log(...this.props.user);
+    // console.log(this.props.signOut({ ...this.props.user }));
+    // console.log(...this.props.user);
   };
 
   // const loginMenu = () => {
@@ -38,7 +38,7 @@ export class UserMenu extends Component {
   //   }
   // };
   render = () => {
-    if (this.props.user.id) {
+    if (this.props.id) {
       return (
         <form>
           <button onClick={e => this.signOutUser(e)}>Log Out</button>
@@ -64,8 +64,8 @@ export const mapStateToProps = state => ({
   user: state.user
 });
 
-export const mapDispatchToProps = dispatch => ({
-  signOut: ({...this.props.user}) = dispatch(signOut({...this.props.user}))
+export const mapDispatchToProps = (dispatch) => ({
+  signOut: (fake) => dispatch(signOut(fake))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
