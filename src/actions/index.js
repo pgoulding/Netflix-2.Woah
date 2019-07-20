c8a5459287c0d2b399578ab3cdb1d689cb685128
Katherine Lewis <kale8432@colorado.edu>
2019-07-20 14:33:07 -0600

    thunks work and removed awesome slider

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,58 +1,54 @@
 export const updateMovies = (movies, genre) => ({
-  type: 'UPDATE_MOVIES',
-  payload: {
-    movies,
-    genre
-  }
+	type: 'UPDATE_MOVIES',
+	payload: {
+		movies,
+		genre
+	}
 });
 
 export const toggleFavorite = id => ({
-  type: 'TOGGLE_FAVORITE',
-  payload: {
-    id
-  }
+	type: 'TOGGLE_FAVORITE',
+	payload: {
+		id
+	}
 });
 
 export const signIn = ({ email, password, id, name }) => ({
-  type: 'SIGN_IN',
-  payload: {
-    email,
-    password,
-    id,
-    name
-  }
+	type: 'SIGN_IN',
+	payload: {
+		email,
+		password,
+		id,
+		name
+	}
 });
 
 export const signOut = state => ({
-  type: 'SIGN_OUT',
-  payload: {
-    ...state
-  }
+	type: 'SIGN_OUT',
+	payload: {
+		...state
+	}
 });
 
-export const createAccount = ({
-  name,
-  password,
-  email
-}) => ({
-  type: 'CREATE_ACCOUNT',
-  payload: {
-    name,
-    password,
-    email
-  }
+export const createAccount = ({ name, password, email }) => ({
+	type: 'CREATE_ACCOUNT',
+	payload: {
+		name,
+		password,
+		email
+	}
 });
 
 export const throwError = error => ({
-  type: 'THROW_ERROR',
-  payload: {
-    error
-  }
+	type: 'THROW_ERROR',
+	payload: {
+		error
+	}
 });
 
 export const isLoading = isLoading => ({
-  type: 'IS_LOADING',
-  payload: {
-    isLoading
-  }
+	type: 'IS_LOADING',
+	payload: {
+		isLoading
+	}
 });
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -7,50 +7,51 @@ import { Route } from 'react-router-dom';
 import { getMovies } from '../../thunks/getMoviesThunk';
 import Header from '../../containers/Header/Header';
 import MainGallery from '../../components/MainGallery/MainGallery';
-import UserSignup from '../UserMenu/UserSignup'
+import UserSignup from '../UserMenu/UserSignup';
 import UserLogin from '../UserMenu/UserLogin';
-// import UserMenu 
+import Error from '../../components/Error/error404';
 
 export class App extends Component {
 	componentDidMount() {
 		this.getDefaultData();
-    };
-	
+	}
 
 	getDefaultData = () => {
-    const startingFetch = ['popular', 'now_playing', 'top_rated'];
-    startingFetch.forEach(genre => {
-      console.log(genre)
-     this.props.getMovies(genre, null);
-      // await this.props.updateMovieState(results, genre);
+		const startingFetch = [ 'popular', 'now_playing', 'top_rated' ];
+		startingFetch.forEach(async genre => {
+			const results = await this.props.getMovies(genre);
 		});
 	};
 
-
 	render() {
 		return (
-      <main>
-        <Header />
-        <Route exact path='/'
-          render={() => (
-            <section>
-              {this.props.movies.popular && <MainGallery movies={this.props.movies.popular} />}
-              {this.props.movies.now_playing && (<Gallery genre={'now_playing'} data={this.props.movies.now_playing} />)}
-              {/* {this.props.movies.popular && ( <Gallery genre={'popular'} data={this.props.movies.popular} />)} */}
-              {this.props.movies.top_rated && (<Gallery genre={'top_rated'} data={this.props.movies.top_rated} />)}
-            </section>
-          )}
-        />
-        {/* <Route path='/categories'
-            render={ () => (
-              <section>
-                {this.props.categories.length && ( <Gallery genre={genre} data={this.props.categories.selected} />)}
-              </section>
-            )}
-          /> */}
-        <Route exact path='/sign_in' render={UserLogin} />
-        <Route exact path='/create_account' component={UserSignup} />
-      </main>
+			<main>
+				<Header />
+				<Route
+					exact
+					path="/"
+					render={() => (
+						<section>
+							{' '}
+							{this.props.movies.now_playing && <MainGallery movies={this.props.movies.now_playing} />}{' '}
+							{this.props.movies.now_playing && (
+								<Gallery genre={'now_playing'} data={this.props.movies.now_playing} />
+							)}{' '}
+							{this.props.movies.popular && <Gallery genre={'popular'} data={this.props.movies.popular} />}{' '}
+							{this.props.movies.top_rated && <Gallery genre={'top_rated'} data={this.props.movies.top_rated} />}{' '}
+						</section>
+					)}
+				/>{' '}
+				{/* <Route path='/categories'
+														                                  render={ () => (
+														                                    <section>
+														                                      {this.props.categories.length && ( <Gallery genre={genre} data={this.props.categories.selected} />)}
+														                                    </section>
+														                                  )}
+														                                /> */}{' '}
+				  <Route exact path="/sign_in" render={UserLogin} />{' '}
+				<Route exact path="/create_account" component={UserSignup} />  {/* <Route component={<Error />} /> */}{' '}
+			</main>
 		);
 	}
 }
@@ -60,8 +61,8 @@ const mapStateToProps = ({ movies }) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getMovies: (genre, url) => dispatch(getMovies(genre, url)),
-  updateMovieState: (results, genre) => dispatch(updateMovies(results, genre))
+	getMovies: (genre, url) => dispatch(getMovies(genre, url)),
+	updateMovieState: (results, genre) => dispatch(updateMovies(results, genre))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
