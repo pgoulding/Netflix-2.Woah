701ef4d471b9bcf36df6201d656c9c59bcba6db3
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-19 20:34:29 -0600

    add card component for displaying category Cards
    Co-authored-by: Katherine Lewis <Kale8432@colorado.edu>
    Co-authored-by: Patrick Goulding <patrickgoulding@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,15 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './Card.css'
 import { sendFavorite } from '../../utils/API/ApiFetch'
+import DetailedMovieCard from '../../containers/DetailedMovieCard'
 
 const Card = ({ movieInfo, user }) => {
   const { title, poster_path } = movieInfo
   const { user_id } = user
   const imageUrl = `http://image.tmdb.org/t/p/w300/${poster_path}`
+
+  const showDetails = (movie) => {
+    return <DetailedMovieCard movie={movie} />
+  } 
+
+
   return (
     <article className="card">
       <h3>{ title }</h3>
       <img alt={ title && ' movie poster' } src={ imageUrl }/>
+      <button onClick={() => showDetails({...movieInfo, imageUrl})}>View Details</button>
       {user.id && <button onClick={() => sendFavorite({ ...movieInfo, user_id})}>Favorite Movie</button>}
     </article>
   )
