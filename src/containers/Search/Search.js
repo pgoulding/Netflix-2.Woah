609566d722f8eac867de27a2a679e6566380423d
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-19 20:29:47 -0600

    add search movie component and method

---
+++ src/containers/Search/Search.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import {getMovies} from '../../utils/API/ApiFetch'
+import apiKey from '../../apikey'
+
+export class Search extends Component {
+  constructor () {
+    super()
+    this.state ={
+      searchInput:''
+    }
+  }
+
+  handleChange=(e) => {
+    this.setState({searchInput: e.target.value})
+  }
+
+  searchMovies = async () => {
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${this.state.searchInput}&page=1&include_adult=false`
+    const result = await getMovies(this.state.searchInput, searchUrl)
+    console.log(result)
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          name='name'
+          placeholder='Search Text'
+          type='text'
+          value={this.state.searchInput}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.searchMovies}>Search</button>
+      </div>
+    )
+  }
+}
+
+export default Search
