0ba6ce992981d94a70ecbbd339f12c25cab169e3
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-21 13:46:13 -0600

    add genrecontainer to list out all available genres with styles
    Co-authored-by: Katherine Lewis <Kale8432@colorado.edu>
    Co-authored-by: Patrick Goulding <patrickgoulding@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/components/GenreContainer/GenreContainer.js
+++ src/components/GenreContainer/GenreContainer.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from '../Card/Card'
+import { findGenres } from '../../utils/API/ApiFetch'
+import {Link} from 'react-router-dom'
+import './GenreContainer.css'
+export class GenreContainer extends Component  {
+  constructor(){
+    super()
+    this.state={
+      genres:[]
+    }
+  }
+  async componentDidMount(){
+    const genres = await findGenres()
+    this.setState({...genres})
+  }
 
-const GenreContainer = (results) => {
-  const details  = results.map(movie => {
-    return <Card movieInfo={movie} />
-  })
-  return (
-    <div>
-      { details }
-    </div>
-  )
+
+
+  populateGenres() {
+    return this.state.genres.map(genre => {
+      return <Link className='category-links' to={`/genre/${genre.name}`}>{genre.name}</Link>
+    })
+  }
+
+  // async handleSubmit (e) {
+  //   e.preventDefault()
+  //   await fetchSingleGenre(e.target.id)
+  //   return 
+  // }
+  // const details  = results.map(movie => {
+
+  //   // return <Card movieInfo={movie} />
+  // })
+  render() {
+    return (
+      <div className='category-link-container'>
+        { this.populateGenres() }
+      </div>
+    )
+  }
 }
 
 export default GenreContainer
