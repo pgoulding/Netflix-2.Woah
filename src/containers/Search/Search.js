8550a69e7e1fee51784e2f81b98a870b5d20c0dd
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-21 17:17:06 -0600

    WIP search movies

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react'
-import { getMovies } from '../../thunks/getMoviesThunk'
-import apiKey from '../../apikey'
-
+import React, { Component } from 'react';
+// import { getMovies } from '../../thunks/getMoviesThunk';
+// import {searchForMovie} from'../../utils/API/ApiFetch'
+// import SearchOutput from './SearchOutput'
+import { Link } from 'react-router-dom'
 export class Search extends Component {
   constructor () {
     super()
     this.state ={
-      searchInput:''
+      searchInput:'',
     }
   }
 
   handleChange=(e) => {
     this.setState({searchInput: e.target.value})
-  }
-
-  searchMovies = async () => {
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${this.state.searchInput}&page=1&include_adult=false`
-    const result = await getMovies(this.state.searchInput, searchUrl)
-    console.log(result)
   }
 
   render() {
@@ -30,7 +25,12 @@ export class Search extends Component {
           value={this.state.searchInput}
           onChange={this.handleChange}
         />
-        <button onClick={this.searchMovies}>Search</button>
+        <Link to={{
+          pathname: '/search',
+          state: {
+            searchInput: this.state.searchInput
+          }
+        }}>Search</Link>
       </div>
     )
   }
