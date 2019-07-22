599676551dc2c540466d07458690a270d15845ea
Katherine Lewis <kale8432@colorado.edu>
2019-07-22 11:38:26 -0600

    testing app

--- src/utils/cleanMovies.js
+++ src/utils/cleanMovies.js
@@ -1,6 +1,14 @@
 export const cleanMovies = (genre, results) => {
   const newResults = results.map(movieData => {
-    return { ...movieData, movie_id: movieData.id, isFavorited: false, genre, poster_path: `http://image.tmdb.org/t/p/w300${movieData.poster_path}`, backdrop_path: `http://image.tmdb.org/t/p/original${movieData.backdrop_path}` };
+    console.log('movie data', movieData)
+    return {
+      ...movieData,
+      movie_id: movieData.id,
+      isFavorited: false,
+      genre,
+      poster_path: `http://image.tmdb.org/t/p/w300${movieData.poster_path}`,
+      backdrop_path: `http://image.tmdb.org/t/p/original${movieData.backdrop_path}`
+    };
   });
   return newResults;
 };
--- src/utils/cleanMovies.test.js
+++ src/utils/cleanMovies.test.js
@@ -3,5 +3,15 @@ import {
 } from './cleanMovies';
 
 describe('cleanMovies', () => {
-  it('should ')
+  it('should return a cleaned movie', () => {
+    const cleanMovie = {
+      movie_id: 420818,
+      isFavorited: false,
+      gnere: "popular",
+      poster_path: "http://image.tmdb.org/t/p/w300\/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg",
+      backdrop_path: "http://image.tmdb.org/t/p/original\/1TUg5pO1VZ4B0Q1amk3OlXvlpXV.jpg"
+    }
+    const expected = cleanMovie(mockMovie);
+    expect(cleanMovie).toEqual(expected);
+  })
 })
