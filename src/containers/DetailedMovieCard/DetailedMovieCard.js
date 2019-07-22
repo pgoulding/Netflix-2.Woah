02cf2ae092df36d73bc1f16576d732d9fc3f8713
baldm0mma <jev.forsberg@gmail.com>
2019-07-22 17:58:10 -0600

    Add styling to card, and more info to specific movie cards

--- src/containers/DetailedMovieCard/DetailedMovieCard.js
+++ src/containers/DetailedMovieCard/DetailedMovieCard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSingleMovie } from '../../utils/API/ApiFetch';
 import './DetailedMovieCard.css'
+import { Link } from 'react-router-dom';
+
 export class DetailedMovieCard extends Component {
   constructor() {
     super();
@@ -14,18 +16,30 @@ export class DetailedMovieCard extends Component {
     const { specificMovie } = this.props;
     const currentMovie = await fetchSingleMovie(specificMovie.id);
     this.setState({ currentMovie });
+    await console.log('curr', currentMovie);
   }
 
   render = () => {
-    const { title, overview, poster_path, vote_average, vote_count } = this.state.currentMovie;
+    const { title, overview, poster_path, vote_average, vote_count, revenue, runtime, homepage, release_date, genres } = this.state.currentMovie;
+    let movieGenres;
+    if (genres) {
+      movieGenres = genres.map(genre => ` ${genre.name},`);
+    }
     return (
       <article className='specific-container'>
         <div className="movie__specific-details">
-          <img alt={overview} src={`http://image.tmdb.org/t/p/w300${poster_path}`} />
+          <div className='image_container'>
+            <img alt={overview} src={`http://image.tmdb.org/t/p/w300${poster_path}`} />
+            <a target='_blank' rel='noopener noreferrer' href={homepage}>Movie Homepage</a>
+          </div>
           <div className="movie-detail-info">
             <h3>{title}</h3>
             <p>{overview}</p>
             <p>Rating: {vote_average} /10</p>
+            <p>Date Released: {release_date}</p>
+            <p>Genres: {movieGenres}</p>
+            <p>Revenue: ${revenue} USD</p>
+            <p>Rumtime: {runtime}</p>
             <p>Times Reviewed: {vote_count}</p>
           </div>
         </div>
