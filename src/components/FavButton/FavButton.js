a6109b5f65d08aa5ce1b1dfee5676fa693550fe0
Katherine Lewis <kale8432@colorado.edu>
2019-07-23 15:04:41 -0600

    fav button tested

--- src/components/FavButton/FavButton.js
+++ src/components/FavButton/FavButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const FavButton = (movieData, user_id, currentUserFavorites) => {
-  const toggle = currentUserFavorites.find(
-    fav => fav.movie_id === movieData.movie_id
-  );
-  return (
-    <button movieData={movieData} user_id={user_id}>
-      {toggle === -1 ? 'Favorite' : 'Unfavorite'}
-    </button>
-  );
+export const FavButton = ({ movieData, user_id, currentUserFavorites }) => {
+	const toggle = currentUserFavorites.find(fav => fav.movie_id === movieData.movie_id);
+	return (
+		<button movieData={movieData} user_id={user_id}>
+			{toggle === -1 ? 'Favorite' : 'Unfavorite'}
+		</button>
+	);
 };
+
+export default FavButton;
