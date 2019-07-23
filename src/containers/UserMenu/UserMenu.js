313f93b9dd78ad461a65f52a8e3cb6ed87abca5f
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-22 19:03:52 -0600

    possibly unnecesary component, please review

--- src/containers/UserMenu/UserMenu.js
+++ src/containers/UserMenu/UserMenu.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react';
-import './UserMenu.css';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { signOut } from '../../actions';
+// import React, { Component } from 'react';
+// import './UserMenu.css';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { signOut } from '../../actions';
 
-export class UserMenu extends Component {
-  signOutUser = async e => {
-    e.preventDefault();
-    const user = {
-      id: '',
-      name: '',
-      password: '',
-      email: ''
-    }
-  };
+// export class UserMenu extends Component {
+//   signOutUser = async e => {
+//     e.preventDefault();
+//     const user = {
+//       id: '',
+//       name: '',
+//       password: '',
+//       email: ''
+//     }
+//   };
 
-  render = () => {
-      return (
-        <form className="user-select-menu">
-          <Link to='/create_account'>
-            <button className="user-select-buttons">Create Account</button>
-          </Link>
-          <Link to="/log_in">
-            <button className="user-select-buttons">Log In</button>
-          </Link>
-        </form>
-      );
-    }
-}
+//   render = () => {
+//       return (
+//         <form className="user-select-menu">
+//           <Link to='/create_account'>
+//             <button className="user-select-buttons">Create Account</button>
+//           </Link>
+//           <Link to="/log_in">
+//             <button className="user-select-buttons">Log In</button>
+//           </Link>
+//           <button onClick={()=> }>Back</button>
+//         </form>
+//       );
+//     }
+// }
 
-export const mapStateToProps = state => ({
-  user: state.user
-});
+// export const mapStateToProps = state => ({
+//   user: state.user
+// });
 
-export const mapDispatchToProps = dispatch => ({
-  signOut: user => dispatch(signOut(user))
-});
+// export const mapDispatchToProps = dispatch => ({
+//   signOut: user => dispatch(signOut(user))
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
+// export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
